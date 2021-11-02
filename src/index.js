82f502d160c91fcc24f04ec928fde281752d9c6d
Liangyu Chen <20095989@mail.wit.ie>
2021-11-02 13:53:57 +0000

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"
 
 const App = () => {
   return (
     <BrowserRouter>
       <SiteHeader />
       <Switch>
+        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
         <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
