4c74ed384ac91c7bc5166611b6e685138ddcdd93
Liangyu Chen <20095989@mail.wit.ie>
2021-12-12 03:09:53 +0000

    Test - favourites-page-test (including delete and add review), fix bugs in home-page-test

--- cypress/integration/favourites-page.spec.js
+++ cypress/integration/favourites-page.spec.js
@@ -1,5 +1,5 @@
 let movies;    // List of movies from TMDB
-
+let favorites;
 // Utility functions
 const filterByTitle = (movieList, string) =>
   movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
@@ -7,13 +7,11 @@ const filterByTitle = (movieList, string) =>
 const filterByGenre = (movieList, genreId) =>
   movieList.filter((m) => m.genre_ids.includes(genreId));
 
-describe("Home Page ", () => {
+describe("Favourites Page ", () => {
   before(() => {
     // Get movies from TMDB and store in movies variable.
     cy.request(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-        "TMDB_KEY"
-      )}&language=en-US&include_adult=false&include_video=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env("TMDB_KEY")}&language=en-US&include_adult=false&include_video=false&page=1`
     )
       .its("body")    // Take the body of HTTP response from TMDB
       .then((response) => {
@@ -21,12 +19,22 @@ describe("Home Page ", () => {
       })
   })
   beforeEach(() => {
-    cy.visit("/")
+    cy.visit("/");
+    cy.get("button[aria-label='add to favorites']").eq(0).click();
+    cy.get("button[aria-label='add to favorites']").eq(1).click();
+    favorites=[movies[0],movies[1]];
+    cy.get(".MuiCardHeader-avatar").should("have.length",favorites.length);
+
+    cy.get("header").find(".MuiToolbar-root").find("button").eq(4).click();
+    cy.get(".MuiCardHeader-content").should("have.length",favorites.length);
+    cy.get(".MuiCardHeader-content").each(($card, index) => {
+      cy.wrap($card).find("p").contains(favorites[index].title);
+    });
   });
 
   describe("Base tests", () => {
     it("displays page header", () => {
-      cy.get("h3").contains("Discover Movies");
+      cy.get("h3").contains("Favourite Movies");
       cy.get("h1").contains("Filter the movies");
     });
   });
@@ -35,7 +43,7 @@ describe("Home Page ", () => {
     describe("By movie title", () => {
      it("should only display movies with m in the title", () => {
        let searchString = "m";
-       let matchingMovies = filterByTitle(movies, searchString);
+       let matchingMovies = filterByTitle(favorites, searchString);
        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
        cy.get(".MuiCardHeader-content").should(
          "have.length",
@@ -47,7 +55,7 @@ describe("Home Page ", () => {
      })
      it("should only display movies with o in the title", () => {
        let searchString = "o";
-       let matchingMovies = filterByTitle(movies, searchString);
+       let matchingMovies = filterByTitle(favorites, searchString);
        cy.get("#filled-search").clear().type(searchString); // Enter m in text box
        cy.get(".MuiCardHeader-content").should(
          "have.length",
@@ -59,7 +67,7 @@ describe("Home Page ", () => {
      });
      it("should only display movies with xyz in the title", ()=>{
          let searchString="xyz";
-         let matchingMovies = filterByTitle(movies, searchString);
+         let matchingMovies = filterByTitle(favorites, searchString);
          cy.get("#filled-search").clear().type(searchString); // Enter xyz in text box
          cy.get(".MuiCardHeader-content").should(
           "have.length",
@@ -72,7 +80,7 @@ describe("Home Page ", () => {
     it("should display movies with the specified genre only", () => {
         const selectedGenreId = 35;
         const selectedGenreText = "Comedy";
-        const matchingMovies = filterByGenre(movies, selectedGenreId);
+        const matchingMovies = filterByGenre(favorites, selectedGenreId);
         cy.get("#genre-select").click();
         cy.get("li").contains(selectedGenreText).click();
         cy.get(".MuiCardHeader-content").should(
@@ -88,10 +96,10 @@ describe("Home Page ", () => {
     describe("By movie title and genre", ()=>{
       it("should only display movies with m in the title and the specified genre", ()=>{
           let searchString="m";
-          let matchingMovies=filterByTitle(movies,searchString);
+          let matchingMovies=filterByTitle(favorites,searchString);
   
-          const selectedGenreId=35;
-          const selectedGenreText="Comedy";
+          const selectedGenreId=12;
+          const selectedGenreText="Adventure";
           matchingMovies=filterByGenre(matchingMovies,selectedGenreId);
   
           cy.get("#filled-search").clear().type(searchString); // Enter m in text box
@@ -110,18 +118,24 @@ describe("Home Page ", () => {
 
 
 
-  describe("Selecting favorite movies", ()=>{
-    it("should display an avatar at the top of the movie card and be added to the Favourite movies page",()=>{
-      cy.get("button[aria-label='add to favorites']").eq(0).click();
-      cy.get("button[aria-label='add to favorites']").eq(1).click();
-      const favorites=[movies[0],movies[1]];
+  describe("Deleting favorite movies", ()=>{
+    it("should delete movie from favourites",()=>{
+      cy.get("button[aria-label='remove from favorites']").eq(0).click();
+      favorites=[favorites[1]];
       cy.get(".MuiCardHeader-avatar").should("have.length",favorites.length);
-
-      cy.get("header").find(".MuiToolbar-root").find("button").eq(4).click();
       cy.get(".MuiCardHeader-content").should("have.length",favorites.length);
       cy.get(".MuiCardHeader-content").each(($card, index) => {
         cy.wrap($card).find("p").contains(favorites[index].title);
       });
+    });
+  });
+
+  describe("Add review to a movie", ()=>{
+    it("should display the add review page of this movie",()=>{
+      cy.get("button[aria-label='remove from favorites']").eq(0).next().click();
+      const movie = favorites[0];
+      cy.get("h3").contains(movie.title);
+      cy.get("h2").contains("Write a review");
     });
   });
 });
